750509e9e44c5ba6c94952d44751197b937df3a0
jakedesjarlais <jake.desjarlais@yahoo.com>
2016-01-24 23:35:18 -0600

    Removing NVIDIA specific scripts

--- bash/p4review.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export P4ROOT=/
-export P4USER=jdesjarlais
-export P4PORT=p4sw:2006
-export P4PASSWD=i2ZTg9y7Gea
-
-perl /sw/main/apps/p4review/p4review.pl -server -s $1 -p4client jd-linux-sw -p4port p4sw:2006
-
-
