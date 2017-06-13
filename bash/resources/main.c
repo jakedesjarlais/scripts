535cfd99223c3bb5f8809adfe461e0a47fda6292
Jake DesJarlais <jake.desjarlais@yahoo.com>
2017-06-13 05:49:50 +0000

    Modified create proj script

---
+++ bash/resources/main.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  printf("HELLO WORlD!!!\n");
+  return 0;
+}
---
+++ bash/resources/main.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+#include <stdlib.h>
+
+int main(void) {
+  std::cout << "HELLO WORLD" << std::endl;
+  return 0;
+}
